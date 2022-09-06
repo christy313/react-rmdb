23636181b9f516fe03f1974bb842386603c1c7e2
christy313 <christycreativity@gmail.com>
2022-09-06 17:20:04 -0400

    Modify ReactDOM.createRoot() in index.js file for update version

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
+
+export const Content = styled.div``;
+
+export const LogoImg = styled.img``;
+
+export const TMDBLogoImg = styled.img``;
