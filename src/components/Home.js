e43d38eb6cec9326ca727956c0ea8d37ec80e0b2
christy313 <christycreativity@gmail.com>
2022-09-07 11:33:38 -0400

    Add useHomeFetch as custom hook to separate and clean Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-      console.log(movies);
-    } catch (err) {
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
+  console.log(state);
 
   return <div>Home Page</div>;
 };
