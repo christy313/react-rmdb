c236222a8e91dae6e64c8ec5d3e96e875da573e5
christy313 <christycreativity@gmail.com>
2022-09-06 20:54:19 -0400

    Create fetchMovies function & add useEffect to fetch API & solve API key invalid problem

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
 
@@ -6,6 +7,23 @@ const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
   return <div>Home Page</div>;
 };
 
