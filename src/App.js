4fcbc100f1307189bb7e20f0bca8a3e8de5ac511
christy313 <christycreativity@gmail.com>
2022-09-07 16:51:46 -0400

    Add logo-click back to homepage & wrap it with Link from react router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { GlobalStyle } from "./GlobalStyle";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 const App = () => (
   <Router>
     <Header />
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
     <GlobalStyle />
   </Router>
