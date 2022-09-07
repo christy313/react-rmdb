e23a02e1f7a46e05dd4ed0a78a29fa367594fa55
christy313 <christycreativity@gmail.com>
2022-09-07 14:56:14 -0400

    Set timer to delay the search when searching the movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,10 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
   console.log(state);
 
   return (
@@ -20,6 +21,7 @@ const Home = () => {
           text={state.results[0].overview}
         />
       )}
+      <SearchBar setSearchTerm={setSearchTerm} />
       <Grid header="Popular Movies">
         {state.results.map(movie => (
           <Thumb
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -9,6 +9,7 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -35,5 +36,5 @@ export const useHomeFetch = () => {
     fetchMovies(1);
   }, []);
 
-  return { state, loading, error };
+  return { state, loading, error, setSearchTerm };
 };
