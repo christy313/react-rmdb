cb2d49a3d45145e0b2fa12baa88432c19bc8204d
christy313 <christycreativity@gmail.com>
2022-09-07 15:26:28 -0400

    Set searchTerm from popular movies to search result

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -33,8 +33,9 @@ export const useHomeFetch = () => {
   };
 
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error, setSearchTerm };
+  return { state, loading, error, searchTerm, setSearchTerm };
 };
