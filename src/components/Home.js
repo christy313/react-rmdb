d1ba1c738bce053fd4168debab84be736e983df7
christy313 <christycreativity@gmail.com>
2022-09-07 14:00:31 -0400

    Add Spinner on the bottom when fetching API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
 import { useHomeFetch } from "../hooks/useHomeFetch";
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
@@ -33,6 +34,7 @@ const Home = () => {
           ></Thumb>
         ))}
       </Grid>
+      <Spinner />
     </>
   );
 };
