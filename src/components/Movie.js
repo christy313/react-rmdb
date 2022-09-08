071f2d5d62d6f3543543d9ec86fe03612e7a3f02
christy313 <christycreativity@gmail.com>
2022-09-08 15:37:34 -0400

    Add BreadCrumb component and pass the props named movieTitle from Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 
+import BreadCrumb from "./BreadCrumb";
+
 import NoImage from "../images/no_image.jpg";
 
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -11,10 +13,12 @@ import { useMovieFetch } from "../hooks/useMovieFetch";
 const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong</div>;
+
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
