8d7357171fcef2784ca95cdf7ad5b096e08ceba1
christy313 <christycreativity@gmail.com>
2022-09-08 15:10:33 -0400

    Get movie id with useParams & create a custom hook useMovieFetch to show single movie info after clicking the movie thumb nail

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+import Grid from "./Grid";
+import Spinner from "./Spinner";
 
-const Movie = () => <div>Movie</div>;
+import NoImage from "../images/no_image.jpg";
+
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  console.log(movie);
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
