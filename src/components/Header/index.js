79d5cd6e5fa7c8aa238be84ee84f011c7ca9c69f
christy313 <christycreativity@gmail.com>
2022-10-18 19:48:50 -0400

    Delete react movie logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
@@ -10,9 +9,8 @@ const Header = () => (
   <Wrapper>
     <Content>
       <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
       </Link>
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
     </Content>
   </Wrapper>
 );
