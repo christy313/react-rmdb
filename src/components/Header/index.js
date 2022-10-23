fb1f7e00c05d2aad04a72b9179a3c7b9209061c5
christy313 <christycreativity@gmail.com>
2022-10-23 12:05:28 -0400

    Add rating

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import RMDBLogo from "../../images/react-movie-logo.svg";
@@ -6,15 +6,31 @@ import TMDBLogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 
-const Header = () => (
-  <Wrapper>
-    <Content>
-      <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
-      </Link>
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
-    </Content>
-  </Wrapper>
-);
+import { Context } from "../../context";
+
+const Header = () => {
+  const [user] = useContext(Context);
+  console.log(user);
+  return (
+    <Wrapper>
+      <Content>
+        <Link to="/">
+          <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        </Link>
+
+        {user ? (
+          <Link to="/">
+            <span>{`Hi, ${user.username}`}</span>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <span>Login</span>
+          </Link>
+        )}
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </Content>
+    </Wrapper>
+  );
+};
 
 export default Header;
