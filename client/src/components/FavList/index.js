d8058b42a90771213db5e683383b1ef2799d34f9
christy313 <christycreativity@gmail.com>
2022-10-26 20:09:41 -0400

    Add to favorite UI

--- client/src/components/FavList/index.js
+++ client/src/components/FavList/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Wrapper, Title, Button } from "./FavList.styles";
 
 function FavList() {
-  return <div>index</div>;
+  return (
+    <Wrapper>
+      <Title>Black Adam</Title>
+      <Button>remove</Button>
+    </Wrapper>
+  );
 }
 
 export default FavList;
