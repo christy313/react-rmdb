36a8bdd7e8e7120bf16510102e7abe930da384c8
christy313 <christycreativity@gmail.com>
2022-10-31 18:19:32 -0400

    Initialize user to undefined

--- src/context.js
+++ src/context.js
@@ -3,11 +3,7 @@ import React, { useState, createContext } from "react";
 export const Context = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    id: "",
-    username: "",
-    email: ""
-  });
+  const [user, setUser] = useState(undefined);
 
   return (
     <Context.Provider value={[user, setUser]}>{children}</Context.Provider>
