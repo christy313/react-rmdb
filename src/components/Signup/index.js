42100f337b7c7b4a4a0524f22501ad71177cdb14
christy313 <christycreativity@gmail.com>
2022-11-01 18:31:04 -0400

    Try railway

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -18,7 +18,7 @@ const Signup = () => {
   const handleSubmit = () => {
     if (!username || !password || !email) return setError(true);
 
-    fetch("https://ilovemovie.herokuapp.com/signup", {
+    fetch("https://react-rmdb-backend-production.up.railway.app/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
